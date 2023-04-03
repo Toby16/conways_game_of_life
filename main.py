da30100c20ffec34f2df094b176879f2469407a9
Toby16 <bolajibaasit14@gmail.com>
2023-04-03 03:43:34 -0400

    Report: included new grid with all values initialized to 0 | used for updating the grid

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 import random
-from print_grid import print_grid
+import print_grid
+
+# function to update the grid after each generations
+def update_grid(grid_struc):
+    # get length of grid's row and colums
+    ROWS = len(grid_struc)
+    COLS = len(grid_struc[0])
+
+    # new 2x2 grid to be updated
+    # all cells initialized to zero
+    new_grid = [[0 for _ in range(COLS)] for _ in range(ROWS)]
 
 
 
@@ -20,3 +30,8 @@ if __name__ == "__main__":
     # print grid in original form (2x2 matrix)
     for i in grid:
         print(i)
+
+    print()
+
+    # testing print_grid() method imported from print_grid module
+    print_grid.print_grid(grid)
